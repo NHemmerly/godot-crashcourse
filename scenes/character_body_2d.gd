77dc0c9fc788b08c6df01b9cca53c4f393a51ba0
Moss Packer <clp1559@rit.edu>
2025-08-06 12:55:30 -0400

    refactored sounds, made sound resume from stop point

--- scenes/character_body_2d.gd
+++ scenes/character_body_2d.gd
@@ -2,30 +2,44 @@ extends CharacterBody2D
 
 var direction = Vector2.ZERO
 var SPEED = 100
-var MOVE_SOUND = preload("res://scrape.mp3")
+var SCRAPE_SOUND = preload("res://scrape.mp3")
 var tween = create_tween()
+
+#Used to detect when the character lands
+var was_in_air = !is_on_floor()
+
+#Used to play through the audio instead of repeating the beginning
+var stop_time = 0
 
 #func fade_out(stream_player):
 #	stream_player.volume
 
-func _physics_process(delta):
+func moving_sound(move_sound):
 	
-	var was_in_air = !is_on_floor()
+	$AudioStreamPlayer2D/Timer.wait_time = move_sound.get_length()
+	if stop_time >= move_sound.get_length():
+		stop_time = 0
+	
+	if is_on_floor() && velocity.x != 0:
+		if !$AudioStreamPlayer2D.is_playing():
+			$AudioStreamPlayer2D.stream = move_sound
+			$AudioStreamPlayer2D.play(2)
+			$AudioStreamPlayer2D/Timer.start()
+	elif $AudioStreamPlayer2D.is_playing():
+		$AudioStreamPlayer2D.stop()
+		stop_time = move_sound.get_length() - $AudioStreamPlayer2D/Timer.time_left
+		print(stop_time)
+
+		
+		
+	#Land sounds
+	#if is_on_floor() && was_in_air:
+		#$AudioStreamPlayer2D.play(0.3)
+
+func _physics_process(delta):
 	
 	#Gravity
 	velocity.y += 2
-	
-	#Move sounds
-	if is_on_floor() && velocity.x != 0:
-		if !$AudioStreamPlayer2D.is_playing():
-			$AudioStreamPlayer2D.stream = MOVE_SOUND
-			$AudioStreamPlayer2D.play()
-	else:
-		$AudioStreamPlayer2D.stop()
-		
-	#Land sounds
-	if is_on_floor() && was_in_air:
-		$AudioStreamPlayer2D.play(0.3)
 	
 	if Input.is_action_just_pressed("Up") && is_on_floor():
 		velocity.y = -1 * SPEED
@@ -47,4 +61,5 @@ func _physics_process(delta):
 		position.x = get_parent().get_node("Spawn").position.x
 		position.y = get_parent().get_node("Spawn").position.y
 	
+	moving_sound(SCRAPE_SOUND)
 	move_and_slide()
